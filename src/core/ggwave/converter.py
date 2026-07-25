dbb717ecf70c21f8431b907f051bbe432d63ec8f
Ben-Beee <yepupuyp@mail.ru>
2026-07-25 20:47:35 +0300

    1

--- src/core/ggwave/converter.py
+++
@@ -1,6 +0,0 @@
-import ggwave
-
-def encoded(file):
-    waveform = ggwave.encode(str(file), protocolId = 1, volume = 20)
-    print(str(file))
-    return waveform
--- src/data/config.py
+++
@@ -1,7 +0,0 @@
-CONFIG = {
-    'File': '2kb.txt',
-    'Path': r'C:/Users/Guest/Desktop/2kb.txt',
-    'Format': 'BIN',
-    'Freq': 44100,
-    'Frame': 1024
-}
